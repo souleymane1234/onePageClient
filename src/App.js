1176afbbda8376af23c10ea18122695250c6afc1
souleymane1234 <konatesouleymane42@gmail.com>
2023-01-25 11:43:05 +0100

    udddd

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Header from './Components/Header';
 import Content from './Components/Content';
 import Footer from './Components/Footer';
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    
-     <React.Fragment>
-       <Content />
-       <Footer />
-    </React.Fragment>
+    <BrowserRouter basename='/onePageClient'>
+        <Content />
+        <Footer />
+    </BrowserRouter>
   );
 }
 
